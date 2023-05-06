a0503b759130e2968bdc8d23957aabc867c1c500
ree4ever2 <ree4ever2@gmail.com>
2023-05-06 13:04:39 +0200

    addthings

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+//
+
+  //$('button').click(function (){
+//$('button').append(addEventListener)
+
+  
+  
+  
+
+  //$("button").click( function() {
+    //alert("Hello World!");
+  
+
+  //$(selector).on(eventType, function);
+
+  $(".gomb").on('click', function () {
+    alert('Igen, bizony, szia!')
+  })
+
+  $('.gomb2').on('click', function (){
+    $('.gomb').text('Szia a Kajojinának!')
+  }
+  
+  )
+
+  $('.gomb3').on('click', function(){
+  $('button').css('background-color', 'orange').text('Fidesz')  
+  
+  }
+  
+  )
